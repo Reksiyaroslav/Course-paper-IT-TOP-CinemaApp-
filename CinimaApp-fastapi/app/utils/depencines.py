3f68a284c9dc588ec9bd944d809fc93357d5a301
Reksiyaroslav <maltzevv.ja@gmail.com>
2026-03-02 00:07:02 +0300

    depensi update

--- CinimaApp-fastapi/app/utils/depencines.py
+++ CinimaApp-fastapi/app/utils/depencines.py
@@ -1,4 +1,4 @@
-from app.service.actor_service import ActorService 
+from app.service.actor_service import ActorService
 from app.service.author_service import AuthorService
 from app.service.coment_service import ComentService
 from app.service.ratingfilms_service import RatingFilmService
@@ -7,19 +7,25 @@ from app.service.user_service import UserService
 from app.utils.comon import SessionDep
 
 
+async def get_user_service(async_session: SessionDep) -> UserService:
+    return UserService(async_session)
 
-async def get_user_service(async_session:SessionDep)   ->UserService:
-    return UserService(async_session) 
 
-async def get_film_service(async_session:SessionDep)    ->FilmService:
-   return FilmService(session=async_session)
-async def get_actor_service(async_session:SessionDep)   ->ActorService:
+async def get_film_service(async_session: SessionDep) -> FilmService:
+    return FilmService(session=async_session)
+
+
+async def get_actor_service(async_session: SessionDep) -> ActorService:
     return ActorService(async_session)
 
-async def get_author_service(async_session:SessionDep)  ->AuthorService:
+
+async def get_author_service(async_session: SessionDep) -> AuthorService:
     return AuthorService(async_session)
-async def get_comment_service(async_session:SessionDep) ->ComentService:
+
+
+async def get_comment_service(async_session: SessionDep) -> ComentService:
     return ComentService(async_session)
-    
-async def get_rating_service(async_session:SessionDep)  ->RatingFilmService:
+
+
+async def get_rating_service(async_session: SessionDep) -> RatingFilmService:
     return RatingFilmService(async_session)
