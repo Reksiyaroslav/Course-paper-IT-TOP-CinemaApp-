05ecd75e23ba1defd5de5c84a21181b60d75f55e
Reksiyaroslav <maltzevv.ja@gmail.com>
2026-03-15 09:28:39 +0300

    remove main.py app create and update main.py dir project

--- CinimaApp-fastapi/main.py
+++ CinimaApp-fastapi/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
 from app.handler.user_api_handler import user_router
 from app.handler.film_api_handler import film_router, get_block_films
 from app.handler.actor_api_handler import actor_router
@@ -8,6 +9,7 @@ from app.handler.author_api_handler import author_router
 from app.handler.coment_api_handler import coment_router
 from app.handler.ratingfilm_handler import rating_router
 from app.handler.ui_api_route import ui_router, teamlates
+
 
 # from app.db.engine import create_tabelS
 # from app.utils.sricpt.py iimport seedn_
@@ -25,7 +27,7 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 # @app.on_event("startup")
 # async def event():
 # await create_tabel()
-
+app.add_middleware(SessionMiddleware,secret_key = "radom_moler")
 
 @app.get("/")
 def hello_people(reqest: Request) -> RedirectResponse:
@@ -33,4 +35,4 @@ def hello_people(reqest: Request) -> RedirectResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=90, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=90)
