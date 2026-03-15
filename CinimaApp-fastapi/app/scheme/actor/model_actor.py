dc1704d042d9c6f2d2173231da87a01cbabb4bb6
Reksiyaroslav <maltzevv.ja@gmail.com>
2026-03-15 09:29:17 +0300

    delete dir sceman create actor author dir

--- CinimaApp-fastapi/app/scheme/actor/model_actor.py
+++ CinimaApp-fastapi/app/scheme/actor/model_actor.py
@@ -4,30 +4,29 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from app.utils.comon import faker, generator_star
 
-
-class ActorCreateRequest(BaseModel):
+class ActorBase(BaseModel):
     fistname: str = Field(default_factory=faker.first_name)
     lastname: str = Field(default_factory=faker.last_name)
     birth_date: datetime.date = Field(default_factory=faker.date_of_birth)
     patronymic: str = Field(default_factory=faker.first_name)
     star: int = Field(gt=1, lt=7, default_factory=generator_star)
 
-
-class ActorUpdateRequest(BaseModel):
-    fistname: Optional[str] = None
+class ActorBaseResponse(BaseModel):
+    fistname: Optional[str] =None 
     lastname: Optional[str] = None
-    patronymic: Optional[str] = None
-    star: Optional[int] = Field(gt=1, lt=7)
-    birth_date: Optional[datetime.date] = None
+    patronymic:  Optional[str] = None
+    star:  Optional[int] = None
+    birth_date:  Optional[datetime.date] = None
+class ActorCreateRequest(ActorBase):
+   pass
 
 
-class ActorResponse(BaseModel):
+class ActorUpdateRequest(ActorBase):
+    pass
+
+
+class ActorResponse(ActorBaseResponse):
     actor_id: uuid.UUID
-    fistname: Optional[str] = None
-    lastname: Optional[str] = None
-    patronymic: Optional[str] = None
-    star: Optional[int] = None
-    birth_date: Optional[datetime.date] = None
     create_at: Optional[datetime.datetime] = None
     update_at: Optional[datetime.datetime] = None
     model_config = {"from_attributes": True}
--- CinimaApp-fastapi/app/scheme/author/model_author.py
+++ CinimaApp-fastapi/app/scheme/author/model_author.py
@@ -4,29 +4,28 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from app.utils.comon import faker, generatao_bio
 
-
-class AuthorCreateRequest(BaseModel):
+class ScreenWriterBase(BaseModel):
     fistname: str = Field(default_factory=faker.first_name)
     lastname: str = Field(default_factory=faker.last_name)
     birth_date: datetime.date = Field(default_factory=faker.date_of_birth)
     patronymic: str = Field(default_factory=faker.first_name)
     bio: str = Field(default_factory=generatao_bio)
-
-
-class AuthorUpdateRequest(BaseModel):
-    fistname: Optional[str] = None
+class ScreenWriterBaseResponse(BaseModel):
+    fistname: Optional[str] =None 
     lastname: Optional[str] = None
-    patronymic: Optional[str] = None
-    bio: Optional[str] = None
-    birth_date: Optional[datetime.date] = None
+    patronymic:  Optional[str] = None
+    bio:  Optional[str] = None
+    birth_date:  Optional[datetime.date] = None
+class AuthorCreateRequest(ScreenWriterBase):
+    pass
 
 
-class AuthorResponse(BaseModel):
+class AuthorUpdateRequest(ScreenWriterBase):
+    pass
+
+
+class AuthorResponse(ScreenWriterBaseResponse):
     author_id: uuid.UUID
-    fistname: Optional[str] = None
-    lastname: Optional[str] = None
-    patronymic: Optional[str] = None
-    bio: Optional[str] = None
     birth_date: Optional[datetime.date] = None
     created_at: Optional[datetime.datetime] = None
     update_at: Optional[datetime.datetime] = None
