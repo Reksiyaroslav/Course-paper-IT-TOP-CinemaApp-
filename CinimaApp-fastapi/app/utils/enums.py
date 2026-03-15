7025b39c485d2d245a24b5008be0f8849deea660
Reksiyaroslav <maltzevv.ja@gmail.com>
2026-03-15 09:32:08 +0300

    delete enums utils

--- CinimaApp-fastapi/app/utils/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-class Role_User(Enum):
-    User = "user"
-    Author ="author"
-    Actor = "actor"
-class Type_Rec(Enum):
-    Like ="like"
-    UnLike = "unlike"
-    Non_Rec = "not_rec"
