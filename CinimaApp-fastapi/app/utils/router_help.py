5252fe2fe90037b2646ff71b39589e74e96496c2
Reksiyaroslav <maltzevv.ja@gmail.com>
2026-04-15 09:31:21 +0300

    routerhelp update

--- CinimaApp-fastapi/app/utils/router_help.py
+++ CinimaApp-fastapi/app/utils/router_help.py
@@ -1,5 +1,6 @@
 from fastapi import Request, Depends, HTTPException
 from datetime import date
+from urllib.parse import urlparse, quote
 from app.utils.depencines import get_user_service, UserService
 from app.scheme.user.model_user import UserResponse
 from app.utils.depencines import (
@@ -24,45 +25,22 @@ async def get_curen_user(
         return None
 
 
-async def get_data_form(
-    country_service: CountryService,
-    film_service: FilmService,
-    actor_service: ActorService,
-    author_service: AuthorService,
-):
-    data = {
-        "countrys": [],
-        "types_film": [],
-        "actors": [],
-        "authors": [],
-    }
-    if country_service:
-        country_relult = await country_service.get_countrys()
-        coyntrys = country_relult.countrys
-        data["countrys"] = coyntrys
-    if actor_service:
-        actor_relult = await actor_service.get_actor_list()
-        actors = actor_relult.actors
-        data["actors"] = actors
-    if author_service:
-        author_relult = await author_service.get_authors()
-        authors = author_relult.author
-        data["authors"] = authors
-    if film_service:
-        type_film_relult = await film_service.get_types_film()
-        types_film = type_film_relult.types_film
-        data["types_film"] = types_film
-    return data
-
-
 def parse_data_or_none(date_str: str, field_name: str = "date"):
-    clean_date_str = date_str.strip()
-    if not date_str or not clean_date_str:
+
+    if not date_str or not date_str.strip():
         return None
     try:
+        clean_date_str = date_str.strip()
         return date.fromisoformat(clean_date_str)
     except ValueError:
         raise HTTPException(
             status_code=400,
             detail=f"Неверный формат '{field_name}'. Ожидается YYYY-MM-DD.",
         )
+
+
+def clean_url_redirect(error_message: str, base_url: str, error_met: str):
+    parse = urlparse(base_url)
+    clean_path = parse.path
+    encode_message = quote(error_message, safe=" ")
+    return f"{clean_path}?{error_met}={encode_message}"
